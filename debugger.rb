487cf575cb04b897e08ec462febc097bec5ea4d8
dongranzero <164931182+dongranzero@users.noreply.github.com>
2024-04-01 23:39:39 +0000

    byebug

---
+++ debugger.rb
@@ -0,0 +1,5 @@
+require "pry-byebug"
+f="your lucky number is "
+l=rand(100)
+byebug
+pp f+l
---
+++ howdy.rb
@@ -0,0 +1,9 @@
+my_string = "Howdy there!"
+pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+
+x=2
+pp x.ordinalize
